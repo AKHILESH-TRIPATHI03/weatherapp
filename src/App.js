7e40c096ed2b4146c459a869e9ba88cf34c7f309
AKHILESH-TRIPATHI03 <akhilesh03tripathi@gmail.com>
2021-05-30 18:49:55 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [city, setCity] = useState(null);
+  const [search, setSearch] = useState("varanasi");
+  const [loading, setloading] = useState(true);
+  useEffect(() => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=88fb926817cfb7b68abbfe463ab29ca8`;
+    fetch(url)
+      .then((data) => {
+        return data.json();
+      })
+      .then((maindata) => {
+        setCity(maindata);
+        setloading(false);
+      });
+  }, [search]);
+  if (loading) return <h1>loading...</h1>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="type-cityname"
+            onChange={(event) => {
+              setSearch(event.target.value);
+            }}
+            onKeyPress={setCity}
+          />
+          {!city ? (
+            <p>no data found</p>
+          ) : (
+            <div>
+              <div className="location-box">
+                <div className="location">{search}</div>
+                {/* <div className="date"></div> */}
+              </div>
+              <div className="weather-box">
+                <div className="temp">{city.main.temp}°c</div>
+                <div className="weather">{city.weather[0].main}rainy</div>
+              </div>
+            </div>
+          )}
+        </div>
+      </main>
     </div>
   );
 }
